5c2f2b01d6313cce9ff33a3485f4e887726614fe
Aakash Akkineni <aakash.kas2@gmail.com>
2025-12-26 19:00:03 -0600

    local host website using vite

---
+++ mysite/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Aakash Akkineni</h1>
+      <p>Engineering Portfolio</p>
+      <p>Project Blog</p>
+      <p>Contact Information</p>      
+      <p>Resume</p>      
+    </div>
+  );
+}
+
+export default App;
